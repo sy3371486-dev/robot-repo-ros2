fe6d7449c6757b5d08b296ffa3545a3e6a5878b4
M11-cell <michael.lythgoe@spaceconcordia.ca>
2025-05-15 10:26:53 -0400

    Update servo node + yaml file

--- src/ceres_servo_controller/src/ceres_servo_control.cpp
+++ src/ceres_servo_controller/src/ceres_servo_control.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "control_msgs/msg/joint_jog.hpp"
 
 
 class GoalTwistPublisher: public rclcpp::Node
@@ -8,8 +9,10 @@ public:
     GoalTwistPublisher(): Node("vel_goal_control_node")
     {
         twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("goal_twist", 10);
+        joint_pub_ = this->create_publisher<control_msgs::msg::JointJog>("joint_states", 10); 
         twist_sub_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
             "/cmd_vel", 10, std::bind(&GoalTwistPublisher::PublishVelocityCommands, this, std::placeholders::_1));
+        joint_sub_ = this->create_subscription<control_msgs::msg::JointJog>("/cmd_vel", 10, std::bind(&GoalTwistPublisher::PublishJointCommands, this, std::placeholders::_1));
         RCLCPP_INFO(this->get_logger(), "GoalTwistPublisher node started");
     }
   
@@ -37,9 +40,44 @@ void PublishVelocityCommands(const geometry_msgs::msg::TwistStamped::SharedPtr m
 
 }
 
+void PublishJointCommands(const control_msgs::msg::JointJog::SharedPtr msg)
+{
+    auto joint_msg = control_msgs::msg::JointJog();
+    joint_msg.header.stamp = this->now();
+    joint_msg.header.frame_id = "base_structure_link";
+    joint_msg.joint_names = {
+        "base_structure_link", 
+        "base_pivot_shoulder_gearbox_joint", 
+        "shoulder_tube_joint", 
+        "bicep_tube_gearbox_joint",
+         "bicep_gearbox_forearm_tube_joint", 
+         "forearm_tube_wrist_gearbox_joint",
+        "wrist_gearbox_wrist_joint", 
+        "gripper_claw_joint"
+    };
+
+    joint_msg.velocities = { 
+        msg->velocities[0], 
+        msg->velocities[1], 
+        msg->velocities[2],
+        msg->velocities[3], 
+        msg->velocities[4], 
+        msg->velocities[5], 
+        msg->velocities[6], 
+        msg->velocities[7]
+    };
+
+    joint_pub_->publish(joint_msg);
+    RCLCPP_INFO(this->get_logger(), "Publishing joint velocity commands to arm:" "[%f, %f, %f, %f, %f, %f, %f, %f]", 
+        joint_msg.velocities[0], joint_msg.velocities[1], joint_msg.velocities[2], 
+        joint_msg.velocities[3], joint_msg.velocities[4], joint_msg.velocities[5],
+        joint_msg.velocities[6], joint_msg.velocities[7]);
+}
 
 rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
+rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_pub_;
 rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_sub_;
+rclcpp::Subscription<control_msgs::msg::JointJog>::SharedPtr joint_sub_;
 rclcpp::TimerBase::SharedPtr timer_;
 
 
@@ -50,7 +88,12 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<GoalTwistPublisher>(); 
-    rclcpp::spin(node);
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
+
+    //rclcpp::spin(node);
+
     rclcpp::shutdown();
     return 0;
 }
