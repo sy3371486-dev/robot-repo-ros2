6fc2ef531d5a17bdcc5b78b774cc57b310bd1fb8
Michael Lythgoe <michael.lythgoe@spaceconcordia.ca>
2025-05-15 10:32:08 -0400

    Update ceres_servo Node

--- src/ceres_servo_controller/src/ceres_servo_control.cpp
+++ src/ceres_servo_controller/src/ceres_servo_control.cpp
@@ -11,34 +11,13 @@ public:
         twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("goal_twist", 10);
         joint_pub_ = this->create_publisher<control_msgs::msg::JointJog>("joint_states", 10); 
         twist_sub_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
-            "/cmd_vel", 10, std::bind(&GoalTwistPublisher::PublishVelocityCommands, this, std::placeholders::_1));
+            "/cmd_vel", 10, std::bind(&GoalTwistPublisher::PublishEEVelocityCommands, this, std::placeholders::_1));
         joint_sub_ = this->create_subscription<control_msgs::msg::JointJog>("/cmd_vel", 10, std::bind(&GoalTwistPublisher::PublishJointCommands, this, std::placeholders::_1));
         RCLCPP_INFO(this->get_logger(), "GoalTwistPublisher node started");
     }
   
     
 private:
-
-void PublishVelocityCommands(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
-{
-    auto twist_msg = geometry_msgs::msg::TwistStamped();
-    twist_msg.header.stamp = this->now();
-    twist_msg.header.frame_id = "base_structure_link";
-    twist_msg.twist.linear.x = msg->twist.linear.x;
-    twist_msg.twist.linear.y = msg->twist.linear.y;
-    twist_msg.twist.linear.z = msg->twist.linear.z;
-    twist_msg.twist.angular.x = msg->twist.angular.x;
-    twist_msg.twist.angular.y = msg->twist.angular.y;
-    twist_msg.twist.angular.z = msg->twist.angular.z;
-    
-    twist_pub_->publish(twist_msg); 
-
-    RCLCPP_INFO(this->get_logger(), "Publishing linear velocity commands to arm:" "[%f, %f, %f]", 
-        twist_msg.twist.linear.x, twist_msg.twist.linear.y, twist_msg.twist.linear.z);    
-    RCLCPP_INFO(this->get_logger(), "Publishing angular velocity commands to arm:" "[%f, %f, %f]",
-        twist_msg.twist.angular.x, twist_msg.twist.angular.y, twist_msg.twist.angular.z);
-
-}
 
 void PublishJointCommands(const control_msgs::msg::JointJog::SharedPtr msg)
 {
@@ -72,6 +51,27 @@ void PublishJointCommands(const control_msgs::msg::JointJog::SharedPtr msg)
         joint_msg.velocities[0], joint_msg.velocities[1], joint_msg.velocities[2], 
         joint_msg.velocities[3], joint_msg.velocities[4], joint_msg.velocities[5],
         joint_msg.velocities[6], joint_msg.velocities[7]);
+} 
+
+void PublishEEVelocityCommands(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+{
+    auto twist_msg = geometry_msgs::msg::TwistStamped();
+    twist_msg.header.stamp = this->now();
+    twist_msg.header.frame_id = "base_structure_link";
+    twist_msg.twist.linear.x = msg->twist.linear.x;
+    twist_msg.twist.linear.y = msg->twist.linear.y;
+    twist_msg.twist.linear.z = msg->twist.linear.z;
+    twist_msg.twist.angular.x = msg->twist.angular.x;
+    twist_msg.twist.angular.y = msg->twist.angular.y;
+    twist_msg.twist.angular.z = msg->twist.angular.z;
+    
+    twist_pub_->publish(twist_msg); 
+
+    RCLCPP_INFO(this->get_logger(), "Publishing linear velocity commands to end effector:" "[%f, %f, %f]", 
+        twist_msg.twist.linear.x, twist_msg.twist.linear.y, twist_msg.twist.linear.z);    
+    RCLCPP_INFO(this->get_logger(), "Publishing angular velocity commands to end effector:" "[%f, %f, %f]",
+        twist_msg.twist.angular.x, twist_msg.twist.angular.y, twist_msg.twist.angular.z);
+
 }
 
 rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
@@ -79,7 +79,6 @@ rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_pub_;
 rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_sub_;
 rclcpp::Subscription<control_msgs::msg::JointJog>::SharedPtr joint_sub_;
 rclcpp::TimerBase::SharedPtr timer_;
-
 
 };
 
