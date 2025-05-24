f9e9589a74a9ec367d7d428b570150af18f6b8e8
Marc Scattolin <marcscattolin@gmail.com>
2025-05-24 14:49:24 -0400

    Modify launch files

--- launch_files/rover-run.sh
+++ launch_files/rover-run.sh
@@ -3,10 +3,13 @@
 # Run setup.sh and wait for it to complete
 ./setup.sh
 
-wait
+# wait
 
 # Launch rover-start in a new terminal
-gnome-terminal -- bash -c "ros2 launch  rover-start.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch rover-start.py"
 
 # Launch aruco_tracker in a new terminal
-gnome-terminal -- bash -c "ros2 launch aruco_tracker.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch ardu-cam.py"
+
+# Launch aruco_tracker in a new terminal
+gnome-terminal -- bash -c "ros2 launch aruco_tracker.py"
